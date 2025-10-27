87e8d2b0abb2ecb039d758a1a78baad5568b8fa8
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-10-27 14:29:42 -0300

    Adicionando relacionamento entre usuários e justificativas

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,13 +2,27 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\TaskReopenJustification;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'description',
         'completed',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reopenJustification() {
+        return $this->hasMany(TaskReopenJustification::class);
+    }
 }
