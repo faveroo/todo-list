7c090eecfa1ebe4766f4f29dc3cd7f2d17f7ec25
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-10-27 14:27:01 -0300

    Adição do método de registro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
@@ -31,6 +33,20 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('tasks.index')->with('success', 'Cadastro realizado com sucesso!');
     }
 }
